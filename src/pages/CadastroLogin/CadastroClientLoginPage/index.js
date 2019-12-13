76a353966beaba5c2d90fcbc4ca2d646a205bb96
guilhermestain <guilhermestain@gmail.com>
2019-12-13 16:54:57 -0200

    cahnges

--- src/pages/CadastroLogin/CadastroClientLoginPage/index.js
+++ src/pages/CadastroLogin/CadastroClientLoginPage/index.js
@@ -3,30 +3,19 @@ import './index.css'
 import { Input, Button, Icon } from 'antd'
 import { Redirect } from 'react-router-dom'
 
-class LoginPage extends Component {
+class CadastroClientLoginPage extends Component {
 
   state = {
     redirect: false,
+    nome: '',
     email: '',
     senha: '',
-    client: true,
+    confirmSenha: '',
   }
 
   onChangeLogin = (e) => {
     this.setState({
       [e.target.name]: e.target.value
-    })
-  }
-
-  changeButtonClient = () => {
-    this.setState({
-      client: true
-    })
-  } 
-
-  changeButtonEmpresa = () => {
-    this.setState({
-      client: false
     })
   }
 
@@ -49,17 +38,24 @@ class LoginPage extends Component {
           <div className='div-main-form-login'>
             <div className='icon-home-login'>
               {this.renderRedirect()}
-              <Icon type="home" style={{ fontSize: '22px', margin: '10px 15px 0 0' }} onClick={this.setRedirect}/>
+              <Icon type="home" style={{ fontSize: '22px', margin: '10px 15px 0 0' }} onClick={this.setRedirect} />
             </div>
             <div className='div-form-login'>
-              {this.state.client ? 
-                <h1 className='h1-bv-login'>Bem vindos ao Connecta Wash Cliente</h1> : 
-                <h1 className='h1-bv-login'>Bem vindos ao Connecta Wash Empresa</h1> 
-              }
+              <h1 className='h1-bv-login'>Cadastro do cliente</h1>
               <p className='p-login'>Obrigado por estar fazendo parte da nossa plataforma online.</p>
               <div className='div-inputs'>
+                <h4 className='p-inputs'>Nome:</h4>
+              <Input
+                placeholder="Digite seu nome"
+                className='input-login'
+                name='nome'
+                value={this.state.nome}
+                onChange={this.onChangeLogin}
+              />
+              </div>
+              <div className='div-inputs'>
                 <h4 className='p-inputs'>Email:</h4>
-                <Input
+              <Input
                 placeholder="Digite seu email"
                 className='input-login'
                 name='email'
@@ -77,23 +73,18 @@ class LoginPage extends Component {
                 onChange={this.onChangeLogin}
               />
               </div>
-              <div className='div-buttons-client-login'>
-                {
-                  this.state.client ? 
-                  <Button className='button-client-login-true' onClick={this.changeButtonClient}>Cliente</Button> 
-                  : <Button className='button-client-login-false' onClick={this.changeButtonClient}>Cliente</Button>
-                } 
-                {
-                  !this.state.client ? 
-                  <Button className='button-empresa-login-true' onClick={this.changeButtonEmpresa} >Empresa</Button> 
-                  : <Button className='button-empresa-login-false' onClick={this.changeButtonEmpresa}>Empresa</Button>
-                } 
+              <div className='div-inputs'>
+                <h4 className='p-inputs'>Conrfimar senha:</h4>
+              <Input.Password
+                placeholder="Confirme sua senha"
+                className='inputsenha-login'
+                name='confirmSenha'
+                value={this.state.confirmSenha}
+                onChange={this.onChangeLogin}
+              />
               </div>
-              <Button className='button-entrar'>Logar</Button>
-              {this.state.client ? 
-              <h4 className='p-login'>Gostaria de se cadastrar? <a href='/cadastroClient'>Clique aqui</a></h4>
-              : <h4 className='p-login'>Gostaria de se cadastrar? <a href='/cadastroEmpresa'>Clique aqui</a></h4>}
-              </div>
+              <Button className='button-entrar'>Cadastrar</Button>
+            </div>
           </div>
         </div>
       </div>
@@ -101,4 +92,4 @@ class LoginPage extends Component {
   }
 }
 
-export default LoginPage
+export default CadastroClientLoginPage
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ import PrivateRoutes from './privateRoutes'
 
 import HomePage from "../pages/Home/HomePage";
 import LoginPage from "../pages/Login/LoginPage";
+import CadastroClientLoginPage from "../pages/CadastroLogin/CadastroClientLoginPage";
 
 const Routes = () => (
   <BrowserRouter >
     <Switch>
       <Route exact path='/home' component={HomePage} />
       <Route exact path='/login' component={LoginPage} />
+      <Route exact path='/cadastroClient' component={CadastroClientLoginPage}/>
       <PrivateRoutes path='/client' />
       <Redirect to='/home' />
     </Switch>
