dac5ed60211f83febd0097b9a1db0b9a0fcd6b61
Jessi Leandro Castro <JLC@localhost.localdomain>
2019-12-16 15:37:48 -0300

    FIX LOGIN

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,25 +3,32 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import uuidValidate from "uuid-validate";
 import * as R from "ramda";
+import { bindActionCreators } from "redux";
 
 import LoginPage from "./LoginPage";
+import { redirect } from "../../components/Menu/MenuRedux/action";
 
 class LoginRoute extends Component {
   hasAuth = R.has("auth");
   hasToken = R.has("token");
 
   render() {
-    if (this.hasAuth(this.props)) {
-      if (this.hasToken(this.props.auth)) {
-        if (uuidValidate(this.props.auth.token)) {
-          this.success();
-
-          return <Redirect to={`/${this.props.auth.typeAccount}/dash`} />;
-        }
-      }
+    if (
+      this.hasAuth(this.props) &&
+      this.hasToken(this.props.auth) &&
+      uuidValidate(this.props.auth.token)
+    ) {
+      this.props.redirect({
+        redirect: `/${this.props.auth.typeAccount}/dash`
+      });
+      return <Redirect to={`/${this.props.auth.typeAccount}/dash`} />;
     }
     return <LoginPage />;
   }
+}
+
+function mapDispacthToProps(dispach) {
+  return bindActionCreators({ redirect }, dispach);
 }
 
 function mapStateToProps(state) {
@@ -30,4 +37,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(LoginRoute);
+export default connect(mapStateToProps, mapDispacthToProps)(LoginRoute);
--- src/routes/privateCompanyRoutes.js
+++ src/routes/privateCompanyRoutes.js
@@ -1,16 +1,63 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 import PagesRoute from "../pages";
 
+import { Logout } from "../pages/Login/LoginRedux/action";
+import { auth } from "../services/login";
+
 class PrivateRoutes extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route path="/company" component={PagesRoute} />
-      </Switch>
+  state = {
+    auth: true
+  };
+  logout = async () => {
+    await this.props.Logout(this.props.auth.token);
+  };
+
+  auth = async () => {
+    const value = {
+      token: this.props.auth.token,
+      username: this.props.auth.username
+    };
+
+    let response = {};
+
+    response = await auth(value).then(resp =>
+      this.setState({
+        auth: resp ? resp.data : false
+      })
     );
+
+    return response;
+  };
+
+  componentDidMount = async () => {
+    await this.auth();
+  };
+  render() {
+    if (this.state.auth) {
+      return (
+        <Switch>
+          <Route path="/company" component={PagesRoute} />
+        </Switch>
+      );
+    } else {
+      this.logout();
+      return <Redirect to="/login" />;
+    }
   }
 }
 
-export default PrivateRoutes;
+function mapDispacthToProps(dispach) {
+  return bindActionCreators({ Logout }, dispach);
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps, mapDispacthToProps)(PrivateRoutes);
